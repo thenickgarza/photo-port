ae65760aa30f843dc812e4ae092682b6cb0934bd
Nick Garza <thenickgarza14@gmail.com>
2022-06-15 23:58:38 -0500

    condtionally renderd the corresponding pictures with the category name in the nav bar

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
+import React from "react";
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Gallery(props) {
-    const currentCategory = {
-        name: "commercial",
-        description:
-        "Phots of grocerystores, food trucks, and other commercial projects",
-    };
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-            <div className="flex-row">
-                <img
-                src={photo}
-                alt="Commercial Example"
-                className="img-thumbnail mx-1"
-                />
-                </div>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList category={currentCategory.name} />
         </section>
-    );
+    )
 }
 
 export default Gallery;
